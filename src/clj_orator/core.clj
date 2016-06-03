1f89c486167358f68dd72d15a2a63bbd36341133
Oliver Wegner <void1976@gmail.com>
2016-06-03 13:32:36 +0200

    initial commit

---
+++ src/clj_orator/core.clj
@@ -0,0 +1,6 @@
+(ns clj-orator.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/clj_orator/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-orator.core-test
+  (:require [clojure.test :refer :all]
+            [clj-orator.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
